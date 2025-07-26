3029350138fd2c52348ec22be9e2170b927ae62a
l-tting <brianletting01@gmail.com>
2025-07-26 08:52:09 +0300

    building rss agg w/chi

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/l_tting/rss-aggregator
+
+go 1.24.4
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-chi/cors v1.2.2
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables
+	godotenv.Load(".env")
+	portstring := os.Getenv("PORT")
+	if portstring == "" {
+		log.Fatal("Port not found in environment")
+	}
+
+	// Set up router
+	router := chi.NewRouter()
+
+	// CORS middleware
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*","http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, 
+	}))
+
+	v1router := chi.NewRouter()
+
+	v1router.Get("/healthz",handlerReadiness)
+
+	v1router.Get("/err",handlerErr)
+
+	router.Mount("/v1",v1router)
+
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from Chi + CORS!"))
+	})
+
+	// Start the server
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portstring,
+	}
+	log.Printf("Starting server on port %v", portstring)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
